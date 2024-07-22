a307c16a520dd8c82f0b829e57c68a5e1ab4630b
Олексій Демидов <agdemidof@gmail.com>
2024-07-22 12:56:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -114,9 +114,9 @@ app.use((err, req, res, next) => {
 });
 
 // Порт, на якому запускається сервер
-const PORT = process.env.PORT || 4444;
+// const PORT = process.env.PORT || 4444;
 
 // Прослуховуємо порт та виводимо повідомлення про запуск сервера
-app.listen(PORT, () => {
+app.listen(process.env.PORT, process.env.HOST, () => {
   console.log(`Server is running on port ${PORT}`);
 });
