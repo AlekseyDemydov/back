d4cc5d9924641879e68008dd0b86e006c9a28695
Олексій Демидов <agdemidof@gmail.com>
2025-05-04 13:27:56 +0300

    fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 import { createProduct, getAllProducts, getOneProduct, deleteProduct, updateProduct } from './ProductController.js';
 import { getMe, createUser} from './UserController.js';
-import {createFeedback, getAllFeedbacks, deleteFeedback} from './FeedbackController copy.js'
+import {createFeedback, getAllFeedbacks, deleteFeedback} from './FeedbackController.js'
 
 export { createFeedback, getAllFeedbacks, deleteFeedback, getMe, createUser, createProduct, getAllProducts, getOneProduct, deleteProduct, updateProduct };
